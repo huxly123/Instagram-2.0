eca9562655d755fd8caf6fe83399d1bfe7bfce63
huxly123 <huxlygerard5@gmail.com>
2021-12-06 15:54:05 +0530

    fds

--- insta/components/Stories.js
+++ insta/components/Stories.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import faker from "faker";
+import Story from "./Story";
 
 function Stories() {
-    return (
-        <div>
-            
-        </div>
-    )
+  const [suggestions, setSuggestions] = useState([]);
+
+  useEffect(() => {
+    const suggestions = [...Array(20)].map((_, i) => ({
+      ...faker.helpers.contextualCard(),
+      id: i,
+    }));
+    setSuggestions(suggestions);
+  }, []);
+
+  return (
+    <div className="flex space-x-2 p-6 bg-white mt-8 border-gray-200 border rounded-sm overflow-x-scroll scrollbar-thin scrollbar-thumb-black">
+      {suggestions.map((profile) => (
+        <Story
+          key={profile.id}
+          img={profile.avatar}
+          username={profile.username}
+        />
+      ))}
+      {/* story */}
+
+      {/* story */}
+      {/* story */}
+      {/* story */}
+      {/* story */}
+      {/* story */}
+    </div>
+  );
 }
 
-export default Stories
+export default Stories;
--- insta/tailwind.config.js
+++ insta/tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
@@ -9,6 +9,8 @@ module.exports = {
     extend: {},
   },
   plugins: [
-    require("@tailwindcss/forms")
+    require("@tailwindcss/forms"),
+    require("tailwind-scrollbar"),
+    require("tailwind-scrollbar-hide"),
   ],
-}
+};
