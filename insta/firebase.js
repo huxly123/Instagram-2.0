7e6ff04c236ea3ce9cbf7583497466c2a68def9c
huxly123 <huxlygerard5@gmail.com>
2021-12-09 02:06:00 +0530

    fds

---
+++ insta/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB3UIMB_Z9ULz-nsJK_60WVMzgS-yp1TRA",
+  authDomain: "instaclone-11122.firebaseapp.com",
+  projectId: "instaclone-11122",
+  storageBucket: "instaclone-11122.appspot.com",
+  messagingSenderId: "861303033675",
+  appId: "1:861303033675:web:3d8d6245bda6fc6baca2ce",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
