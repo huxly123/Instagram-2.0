9f7e9de282365c6d2113d27c2f51adeae41e360f
huxly123 <huxlygerard5@gmail.com>
2021-12-09 16:48:11 +0530

    fds

--- insta/pages/auth/signin.js
+++ insta/pages/auth/signin.js
@@ -1,14 +1,29 @@
-import { async } from '@firebase/util'
+import {getProviders,signIn as SignIntoProvider} from "next-auth/react"
+
 import React from 'react'
 
-export const signin=()=> {
+ const signin=({providers})=> {
     return (
-        <div>
-            signin
-        </div>
-    )
+      <>
+        {Object.values(providers).map((provider) => (
+          <div key={provider.name}>
+            <button onClick={() => SignIntoProvider(provider.id)}>
+              Sign in with {provider.name}
+            </button>
+          </div>
+        ))}
+      </>
+    );
 }
 export async function getServerSideProps() {
-    
+    const providers =await getProviders();
+
+    return {
+        props: {
+        providers
+    }
 }
 
+}
+
+export default signin
