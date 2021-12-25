ff6908c277e93ebce08f443c102e9990f9845f7e
huxly123 <huxlygerard5@gmail.com>
2021-12-25 19:46:12 +0530

    insta

--- insta/components/Post.js
+++ insta/components/Post.js
@@ -23,6 +23,7 @@ import {
   serverTimestamp,
 } from "@firebase/firestore";
 import { db } from "../firebase";
+import Moment from "react-moment";
 
 function Post({ id, username, userImg, img, caption }) {
   const { data: session } = useSession();
@@ -48,10 +49,11 @@ function Post({ id, username, userImg, img, caption }) {
     await addDoc(collection(db, "posts", id, "comments"), {
       comment: commentToSend,
       username: session.user.username,
+      userImage:session.user.image,
       timestamp: serverTimestamp(),
     });
   };
-
+console.log(comments);
   return (
     <div className="bg-white my-7 border rounded-sm">
       {/* Header */}
@@ -88,6 +90,27 @@ function Post({ id, username, userImg, img, caption }) {
       </p>
 
       {/* comment */}
+      {comments.length > 0 && (
+        <div className="ml-10 h-20 overflow-y-scroll scrollbar-thumb-black scrollbar-thin">
+          {comments.map((comment) => (
+            <div key={comment.id} className="flex items-center space-x-2 mb-3">
+              <img
+                className="h-7 rounded-full"
+                src={comment.data().userImage}
+                alt=""
+              />
+              <p className="text-sm flex-1">
+                <span className="font-bold">{comment.data().username}</span>
+                {comment.data().comment}
+              </p>
+              <Moment fromNow className="pr-5 text-xs">
+                {comment.data().timestamp?.toDate()}
+              </Moment>
+            </div>
+          ))}
+        </div>
+      )}
+
       {/* input box */}
       {session && (
         <form className="flex items-center p-4 ">
